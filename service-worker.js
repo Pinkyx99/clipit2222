7993a0d144eed3b266e30116ce1b63099df5a6e6
Pinkyx99 <alijaramill@gmail.com>
2025-07-22 02:40:49 +0200

    a
    a

---
+++ service-worker.js
@@ -0,0 +1,62 @@
+
+const CACHE_NAME = 'clip-it-tycoon-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  // Note: In a real build process, you would add the paths to your JS and CSS bundles.
+  // For this setup, we assume the browser caches the tailwind CDN script.
+  // We will cache other assets as they are requested.
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          (response) => {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              if (event.request.url.startsWith('https://picsum.photos')) {
+                 // Don't cache opaque responses for picsum, just serve them.
+                 return response;
+              }
+            }
+            
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          }
+        );
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
